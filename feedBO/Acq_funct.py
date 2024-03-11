8cbe0d0564b83ae6f27562e88fc676c33d6b05bc
gdug03 <gdug03@gmail.com>
2024-03-11 10:39:36 -0500

    init commit

--- feedBO/Acq_funct.py
+++ feedBO/Acq_funct.py
@@ -8,8 +8,8 @@ from botorch.utils.multi_objective.box_decompositions.non_dominated import (
 from botorch.acquisition.multi_objective.monte_carlo import qExpectedHypervolumeImprovement, qNoisyExpectedHypervolumeImprovement
 from botorch.optim.optimize import optimize_acqf
 
-from feedBO.constraints import Pena_constant_constraints
-from feedBO.pena_function import Pena_func
+#from feedBO.constraints import Pena_constant_constraints
+from feedBO.pena_function import (Pena_func, Pena_constant_constraints)
 
 tkwargs = {"dtype": torch.float64,
     "device": torch.device("cuda:0" if torch.cuda.is_available() else "cpu"),
