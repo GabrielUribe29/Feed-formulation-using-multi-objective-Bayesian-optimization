1843ac641aa68bfd32073f0d899147810570b7da
gdug03 <gdug03@gmail.com>
2024-03-11 11:28:40 -0500

    init commit

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ mll, model =initialize_model(x,y)
 inc, eqc=Pena_constant_constraints()
 #sampler=SobolQMCNormalSampler(512)
 #optimizer=optimize_qNehvi_and_get_observation(model=model, sampler=sampler, train_x=x)
-#print(BO_feed(Seeds=Seeds, Init_samples=10, num_exp=1))
+print(BO_feed(Seeds=Seeds, Init_samples=10, num_exp=1))
 print(inc)
 
 
